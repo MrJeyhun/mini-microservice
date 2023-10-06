62b3b7c26fb78abd78b4bb3871726bfa5286a57e
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-10-06 15:47:23 +0300

    added query service

---
+++ query/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.status(200).send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  switch (type) {
+    case "PostCreated": {
+      const { id, title } = data;
+      posts[id] = { id, title, comments: [] };
+      break;
+    }
+    case "CommentCreated": {
+      const { id, content, postId } = data;
+      const post = posts[postId];
+      post.comments.push({ id, content });
+      break;
+    }
+    default: {
+      console.error("Unknown type");
+    }
+  }
+
+  console.log("Posts: ", posts);
+
+  res.send({});
+});
+
+app.listen(4002, () => {
+  console.log("Listening on 4002");
+});
